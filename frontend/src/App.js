77bb5f2ae1e8e10e9c8d383225315a8a7c497605
phonghp69 <phongdthe140091@gmail.com>
2022-03-26 02:54:23 +0700

    update font-end login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  createTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./Layouts/NavBar/NavBar";
+import "./App.css";
+import Footer from "./Layouts/Footer/Footer";
+import HomePage from "./Pages/HomePage/HomePage";
+import LoginPage from "./Pages/LoginPage/LoginPage";
+
+
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2e1667",
+    },
+    secondary: {
+      main: "#c7d8ed",
+    },
+  },
+  typography: {
+    fontFamily: ["Roboto"],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: "2rem",
+    },
+    h5: {
+      fontWeight: 100,
+      lineHeight: "2rem",
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <NavBar />
+        <Routes>
+        <Route exact path = "/" element={<HomePage/>} />
+        {/* <Route path = "/posts" exact element={<PostsPage/>} />
+        <Route path = "/posts/:id" element={<PostDetail/>} />
+        <Route path = "/profile" element={<ProfilePage/>}/> */}
+        <Route path = "/login" element={<LoginPage/>} />
+      </Routes>
+        <div >
+          <Footer />
+        </div>
+      </ThemeProvider>
+      
     </div>
   );
 }
