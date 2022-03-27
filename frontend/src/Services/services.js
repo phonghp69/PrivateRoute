eded1aa48e92cab5fe24b199a2a7bc11bd429bc4
hoang-debug <campus199@gmail.com>
2022-03-27 21:44:47 +0700

    Auth service and helpers

--- frontend/src/Services/services.js
+++
@@ -1,6 +0,0 @@
-import httpClient from '../httpClient/httpClient'
-import { END_POINT } from '../httpClient/config'
-
-export const loginService = ()=>{
-    return httpClient.get(END_POINT.login)
-}
